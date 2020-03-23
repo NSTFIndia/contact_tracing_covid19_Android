42cb17f1daf39622a56e5491a26b86186fccf381
Tej Pydipalli <tej@edwisely.com>
2020-03-23 11:07:19 +0530

    Initial commit with NearBy API integrated.

---
+++ app/src/main/java/com/google/android/gms/nearby/messages/samples/nearbydevices/TurnOnBluetoothActivity.java
@@ -0,0 +1,15 @@
+package com.google.android.gms.nearby.messages.samples.nearbydevices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TurnOnBluetoothActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_turn_on_bluetooth);
+
+    }
+
+}
